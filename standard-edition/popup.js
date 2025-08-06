569b7dfcb58b2899c2dba40b6448d7415babc6a4
marcelopbg <mpbguima@gmail.com>
2025-08-06 10:22:05 -0300

    fix AI slop and add host_permissions to manifest

--- standard-edition/popup.js
+++ standard-edition/popup.js
@@ -1,1 +1,12 @@
-// ...existing code...
+const options = {};
+const optionsForm = document.getElementById("optionsForm");
+
+optionsForm.isShortsBlocked.addEventListener("change", (event) => {
+    options.isShortsBlocked = event.target.checked;
+    chrome.storage.sync.set({ options });
+});
+
+chrome.storage.sync.get("options").then(data => {
+    Object.assign(options, data.options);
+    optionsForm.isShortsBlocked.checked = Boolean(options.isShortsBlocked);
+})
